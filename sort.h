fbd6b10329d673ccf805ce45c958fdfabf41fd19
Drreey <erzuahirene@gmail.com>
2023-11-21 13:14:43 -0800

    update

--- sort.h
+++ sort.h
@@ -27,5 +27,5 @@ void quick_sort(int *array, size_t size);
 void quick_sort_recursive(int *array, int low, int high, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
 void swap(int *a, int *b);
-
+void shell_sort(int *array, size_t size);
 #endif

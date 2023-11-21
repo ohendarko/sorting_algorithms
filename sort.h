520aa9df723f6ce067290cdaf020d01bc49f2dd6
Drreey <erzuahirene@gmail.com>
2023-11-21 13:24:49 -0800

    update

--- sort.h
+++ sort.h
@@ -28,4 +28,7 @@ void quick_sort_recursive(int *array, int low, int high, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
 void swap(int *a, int *b);
 void shell_sort(int *array, size_t size);
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
 #endif
